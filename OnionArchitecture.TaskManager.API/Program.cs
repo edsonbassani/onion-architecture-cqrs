59749c4c09608a4ad885cb73f275ebae2a08df51
edsonbassaniflynn <edson.bassani@flynncompanies.com>
2024-05-26 10:17:00 -0700

    Dependency Injection

--- OnionArchitecture.TaskManager.API/Program.cs
+++ OnionArchitecture.TaskManager.API/Program.cs
@@ -1,5 +1,11 @@
 
 using Microsoft.EntityFrameworkCore;
+using OnionArchitecture.TaskManager.Application.Handlers.CommandHandlers.Project;
+using OnionArchitecture.TaskManager.Application.Handlers.QueryHandlers.Project;
+using OnionArchitecture.TaskManager.Application.Handlers.CommandHandlers.ProjectTask;
+using OnionArchitecture.TaskManager.Application.Handlers.QueryHandlers.ProjectTask;
+using OnionArchitecture.TaskManager.Application.Handlers.CommandHandlers.User;
+using OnionArchitecture.TaskManager.Application.Handlers.QueryHandlers.User;
 using OnionArchitecture.TaskManager.Application.Interfaces;
 using OnionArchitecture.TaskManager.Application.Services;
 using OnionArchitecture.TaskManager.Core.Interfaces;
@@ -12,14 +18,31 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
-builder.Services.AddScoped<IProjectService, ProjectService>();
-builder.Services.AddScoped<IProjectTaskService, ProjectTaskService>();
-builder.Services.AddScoped<IUserService, UserService>();
-//builder.Services.AddScoped<IProjectRepository, ProjectRepository>();
-//builder.Services.AddScoped<IProjectTasksRepository, ProjectTasksRepository>();
-
 builder.Services.AddControllers();
+
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+builder.Services.AddTransient<IProjectService, ProjectService>();
+builder.Services.AddTransient<IProjectTaskService, ProjectTaskService>();
+builder.Services.AddTransient<IUserService, UserService>();
+
+builder.Services.AddTransient<CreateProjectCommandHandler>();
+builder.Services.AddTransient<UpdateProjectCommandHandler>();
+builder.Services.AddTransient<DeleteProjectCommandHandler>();
+builder.Services.AddTransient<GetProjectByIdQueryHandler>();
+builder.Services.AddTransient<GetAllProjectsQueryHandler>();
+
+builder.Services.AddTransient<CreateProjectTaskCommandHandler>();
+builder.Services.AddTransient<UpdateProjectTaskCommandHandler>();
+builder.Services.AddTransient<DeleteProjectTaskCommandHandler>();
+builder.Services.AddTransient<GetProjectTaskByIdQueryHandler>();
+builder.Services.AddTransient<GetAllProjectTasksQueryHandler>();
+
+builder.Services.AddTransient<CreateUserCommandHandler>();
+builder.Services.AddTransient<UpdateUserCommandHandler>();
+builder.Services.AddTransient<DeleteUserCommandHandler>();
+builder.Services.AddTransient<GetUserByIdQueryHandler>();
+builder.Services.AddTransient<GetAllUsersQueryHandler>();
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
